ba6ad47a8e8be8daa5fc2c16020b81041a69523a
Thomas Carré <100042304+ThomaSneazy@users.noreply.github.com>
2024-08-15 14:52:39 +0000

    gsap anim video wrapper

--- script.js
+++ script.js
@@ -1,4 +1,135 @@
 document.addEventListener("DOMContentLoaded", () => {
+  const videoWrappers = document.querySelectorAll(".home-video__wrapper");
+
+  videoWrappers.forEach((wrapper) => {
+    const itemWrappers = wrapper.querySelectorAll(".home-video__item__wrapper");
+    const button = wrapper.querySelector(".button-icon.is-video-section");
+    const title = wrapper.querySelector(".home-video__btn .heading-style-h3");
+    const videoBe = wrapper.querySelectorAll(".video-be");
+    const openButton = wrapper.querySelector(".home-video-btn__open");
+    const openIcon = openButton.querySelector(".icon-arrow");
+    const closeIcon = openButton.querySelector(".icon-arrow.is-close");
+
+    let activeVideoIndex = Math.floor(Math.random() * videoBe.length);
+
+    gsap.set(itemWrappers, { height: "4.72rem", width: "auto" });
+    gsap.set(button, { opacity: 0 });
+    gsap.set(title, { opacity: 1 });
+    gsap.set(videoBe, { opacity: 0 });
+    gsap.set(openButton, { left: "0.4rem" });
+    gsap.set(closeIcon, { opacity: 0 });
+
+    const tl = gsap.timeline({ paused: true });
+
+    tl.to(itemWrappers, {
+      height: "32rem",
+      width: "20rem",
+      duration: 0.8,
+      ease: "power3.inOut",
+    })
+      .to(
+        button,
+        {
+          opacity: 1,
+          duration: 0.4,
+          ease: "power2.inOut",
+          onStart: () => button.classList.add("pointer-events-auto"),
+        },
+        "-=0.6",
+      )
+      .to(
+        title,
+        {
+          opacity: 0,
+          duration: 0.4,
+          ease: "power2.inOut",
+        },
+        "-=0.4",
+      )
+      .to(
+        videoBe[activeVideoIndex],
+        {
+          opacity: 1,
+          duration: 0.4,
+          ease: "power2.inOut",
+        },
+        "-=0.4",
+      )
+      .to(
+        openButton,
+        {
+          left: "98%",
+          xPercent: -100,
+          duration: 0.6,
+          ease: "power3.inOut",
+        },
+        "-=0.3",
+      )
+      .to(
+        openIcon,
+        {
+          opacity: 0,
+          duration: 0.2,
+          ease: "power2.inOut",
+        },
+        "-=0.3",
+      )
+      .to(
+        closeIcon,
+        {
+          opacity: 1,
+          duration: 0.2,
+          ease: "power2.inOut",
+        },
+        "-=0.2",
+      );
+
+    const toggleVideo = () => {
+      gsap.to(videoBe[activeVideoIndex], {
+        opacity: 0,
+        duration: 0.4,
+        ease: "power2.inOut",
+      });
+      activeVideoIndex = (activeVideoIndex + 1) % videoBe.length;
+      gsap.to(videoBe[activeVideoIndex], {
+        opacity: 1,
+        duration: 0.4,
+        ease: "power2.inOut",
+      });
+    };
+
+    const hideAllVideos = () => {
+      gsap.to(videoBe, { opacity: 0, duration: 0.4, ease: "power2.inOut" });
+    };
+
+    wrapper.addEventListener("mouseenter", () => {
+      tl.play();
+      wrapper.classList.add("z-index-2");
+    });
+
+    wrapper.addEventListener("mouseleave", () => {
+      tl.reverse();
+      hideAllVideos();
+      wrapper.classList.remove("z-index-2");
+      button.classList.remove("pointer-events-auto");
+    });
+
+    openButton.addEventListener("click", (e) => {
+      e.stopPropagation();
+      if (tl.progress() === 0 || tl.reversed()) {
+        tl.play();
+        wrapper.classList.add("z-index-2");
+      } else {
+        tl.reverse();
+        hideAllVideos();
+        wrapper.classList.remove("z-index-2");
+        button.classList.remove("pointer-events-auto");
+      }
+    });
+  });
+});
+//////////////////////COMMENTAIRE ICI//////////////////////
+document.addEventListener("DOMContentLoaded", () => {
   const buttonDrop = document.querySelector(".button-drop");
   const dropdownWrapper = document.querySelector(".dropdown__list__wrapper");
   const dropdownItems = dropdownWrapper.querySelectorAll(".dropdown__item");
