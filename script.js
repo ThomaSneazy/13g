7060b79748f38c40f6c97ce5259bcbc01f436e21
Thomas Carré <100042304+ThomaSneazy@users.noreply.github.com>
2024-08-26 16:04:25 +0000

    init swiper with flip gsap error change to transform

--- script.js
+++ script.js
@@ -1,26 +1,38 @@
 $(document).ready(function () {
-  var swiper = new Swiper(".temoignage-home", {
-    slidesPerView: "auto",
-    centeredSlides: true,
-    spaceBetween: 30,
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
-    on: {
-      init: function () {
-        applyReviewCardStyles();
-        showRelevantCards();
+  var swiper;
+
+  function initSwiper() {
+    // Initialiser l'opacité des diapositives à 0 et les déplacer vers le bas, sauf la slide active
+    gsap.set(".swiper-slide.temoignage-home:not(.swiper-slide-active)", {
+      opacity: 0,
+      y: 40,
+    });
+    // Initialiser seulement l'opacité de la slide active à 0, laisser Y à 0
+    gsap.set(".swiper-slide.temoignage-home.swiper-slide-active", {
+      opacity: 0,
+      y: 0,
+    });
+
+    swiper = new Swiper(".swiper.temoignage-home", {
+      slidesPerView: "auto",
+      centeredSlides: true,
+      spaceBetween: 30,
+      loop: true,
+      grabCursor: true,
+      on: {
+        init: function () {
+          applyReviewCardStyles();
+          showRelevantCards();
+        },
       },
-    },
-  });
+    });
+  }
 
   function applyReviewCardStyles() {
     $(".card-review").each(function () {
       var $card = $(this);
       var source = $card.data("source");
       var className = "";
-
       switch (source) {
         case "linkedin":
           className = "linkedin";
@@ -32,15 +44,12 @@ $(document).ready(function () {
           className = "treize-g";
           break;
       }
-
       if (className) {
         $card.addClass(className);
-
         $card
           .find(".content-review, .info-titre, .poste-review")
           .addClass(className);
       }
-
       handleStarRating($card);
     });
   }
@@ -48,7 +57,6 @@ $(document).ready(function () {
   function handleStarRating($card) {
     var $starWrapper = $card.find(".etoile-wrap");
     var starRating = parseInt($starWrapper.data("star"));
-
     if (starRating >= 1 && starRating <= 5) {
       $starWrapper.find(".etoile-1").each(function (index) {
         if (index < starRating) {
@@ -61,17 +69,170 @@ $(document).ready(function () {
   }
 
   function showRelevantCards() {
-    // Cacher toutes les cartes par défaut
     $(".card-review").hide();
-
-    // Afficher uniquement les cartes avec les classes spécifiées
     $(
       ".card-review.linkedin, .card-review.google, .card-review.treize-g",
     ).show();
-
-    // Mettre à jour Swiper après avoir modifié la visibilité des slides
-    swiper.update();
+    if (swiper && typeof swiper.update === "function") {
+      swiper.update();
+    }
   }
+
+  function animateSlides() {
+    const slides = document.querySelectorAll(".swiper-slide.temoignage-home");
+
+    slides.forEach((slide) => {
+      if (slide.classList.contains("swiper-slide-active")) {
+        // Animation pour la diapositive active (seulement opacité)
+        gsap.to(slide, {
+          opacity: 1,
+          y: 0,
+          duration: 1.2,
+          ease: "power2.out",
+        });
+      } else {
+        // Animation pour les autres diapositives (opacité + mouvement Y)
+        gsap.to(slide, {
+          opacity: 1, // Opacité réduite pour les autres diapositives
+          y: 40, // Mouvement Y
+          duration: 1.2,
+          ease: "power2.out",
+        });
+      }
+    });
+  }
+
+  function initScrollAnimation() {
+    gsap.registerPlugin(ScrollTrigger);
+
+    const firstCard = document.querySelector(".first-card");
+    const contentReview = firstCard.querySelector(".content-review.is_first");
+    const p = contentReview.querySelector("p");
+    const texts = contentReview.querySelectorAll("*");
+    const textSizeMedium = firstCard.querySelector(
+      ".text-size-medium.is_first",
+    );
+    const posteReview = firstCard.querySelector(".poste-review.is_first");
+    const infoTitre = firstCard.querySelector(".info-titre");
+    const starWrapper = firstCard.querySelector(".star-treize-g__wrapper");
+
+    const tl = gsap.timeline({
+      scrollTrigger: {
+        trigger: ".first-slide",
+        start: "top 10%",
+        end: "bottom top",
+        markers: true,
+      },
+    });
+
+    tl.to(firstCard, {
+      width: "34.72rem",
+      height: "auto",
+      minHeight: "12rem",
+      borderRadius: "0.5rem",
+      backgroundColor: "#2b2b2b",
+      duration: 1.2,
+      ease: "power2.inOut",
+    })
+      .to(
+        p,
+        {
+          fontSize: "0.9rem",
+          duration: 1,
+          ease: "power1.inOut",
+        },
+        "-=1.8",
+      )
+      .to(
+        texts,
+        {
+          fontSize: "0.9rem",
+          duration: 1,
+          ease: "power1.inOut",
+        },
+        "-=1.3",
+      )
+      .to(
+        textSizeMedium,
+        {
+          fontSize: "1.67rem",
+          duration: 1,
+          ease: "power1.inOut",
+        },
+        "-=1.3",
+      )
+      .to(
+        posteReview,
+        {
+          fontSize: "0.9rem",
+          duration: 1,
+          ease: "power1.inOut",
+        },
+        "-=1.3",
+      )
+      .to(
+        starWrapper,
+        {
+          fontSize: "0.9rem",
+          duration: 1,
+          ease: "power1.inOut",
+        },
+        "-=1.3",
+      )
+      .to(
+        infoTitre,
+        {
+          opacity: 0,
+          paddingLeft: "1.67rem",
+          minWidth: "0",
+          duration: 0.8,
+          ease: "power1.inOut",
+        },
+        0.3,
+      )
+      .to(
+        contentReview,
+        {
+          opacity: 0,
+          duration: 0.8,
+          ease: "power1.inOut",
+        },
+        0.2,
+      )
+      .to(
+        ".swiper-slide.temoignage-home.swiper-slide-active",
+        {
+          opacity: 1,
+          y: 0,
+          duration: 0.3,
+          ease: "power2.inOut",
+        },
+        "-=0.5",
+      ) // Commencer l'animation de la slide active 0.5s avant la fin de firstCard
+      .to(
+        firstCard,
+        {
+          opacity: 0,
+          duration: 0.3,
+          ease: "power1.inOut",
+        },
+        "-=0.2",
+      )
+      .to(
+        ".swiper-slide.temoignage-home:not(.swiper-slide-active)",
+        {
+          opacity: 1,
+          y: 0, // Réintégration du mouvement Y de 40 à 0
+          duration: 1.2,
+          ease: "power2.out",
+          stagger: 0.2,
+        },
+        "-=0.3",
+      ); // Commencer l'animation des autres slides un peu avant la fin de la transition de firstCard
+  }
+
+  initSwiper();
+  initScrollAnimation();
 });
 ///////////////HOVER AND CLICK BUTTON BLUR BG////////////////////
 $(document).ready(function () {
